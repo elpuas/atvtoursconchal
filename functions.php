ea59f98785f8546dfbc22423be14c079c7821511
elpuas <elpuas@gmail.com>
2018-08-17 14:32:36 -0400

    remove woocommerce tabs

--- functions.php
+++ functions.php
@@ -62,5 +62,5 @@ add_filter( 'woocommerce_product_tabs', 'remove_woocommerce_product_tabs', 98 );
  * Hook in each tabs callback function after single content.
  */
 add_action( 'woocommerce_after_single_product_summary', 'woocommerce_product_description_tab' );
-add_action( 'woocommerce_after_single_product_summary', 'woocommerce_product_additional_information_tab' );
-add_action( 'woocommerce_after_single_product_summary', 'comments_template' );
+// add_action( 'woocommerce_after_single_product_summary', 'woocommerce_product_additional_information_tab' );
+// add_action( 'woocommerce_after_single_product_summary', 'comments_template' );
