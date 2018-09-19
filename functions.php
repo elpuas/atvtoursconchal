7d364ef3bcca4714e12bdf315c48519549a90fd2
elpuas <elpuas@gmail.com>
2018-09-19 17:16:45 -0400

    updates: 5

--- functions.php
+++ functions.php
@@ -96,3 +96,41 @@ function atv_price_range( $wcv_price, $product ) {
  
 add_filter( 'woocommerce_variable_sale_price_html', 'atv_price_range', 10, 2 );
 add_filter( 'woocommerce_variable_price_html', 'atv_price_range', 10, 2 );
+
+/* Auto populate gravity forms drop down */
+
+add_filter( 'gform_pre_render_51', 'populate_posts' );
+
+add_filter( 'gform_pre_validation_51', 'populate_posts' );
+
+add_filter( 'gform_pre_submission_filter_51', 'populate_posts' );
+
+add_filter( 'gform_admin_pre_render_51', 'populate_posts' );
+
+function populate_posts( $form ) {
+ 
+    foreach ( $form['fields'] as &$field ) {
+ 
+        if ( $field->type != 'select' || strpos( $field->cssClass, 'populate-posts' ) === false ) {
+            continue;
+        }
+ 
+        // you can add additional parameters here to alter the posts that are retrieved
+        // more info: http://codex.wordpress.org/Template_Tags/get_posts
+        $posts = get_posts( 'numberposts=-1&post_status=publish' );
+ 
+        $choices = array();
+ 
+        foreach ( $posts as $post ) {
+            $choices[] = array( 'text' => $post->post_title, 'value' => $post->post_title );
+        }
+ 
+        // update 'Select a Post' to whatever you'd like the instructive option to be
+		$field->placeholder = 'Select a Tour';
+		
+        $field->choices = $choices;
+ 
+    }
+ 
+    return $form;
+}
